2edf7c6042f470402a18832cfd88e5086198feab
Enzo Horquin <ehorquin@alumnos.exa.unicen.edu.ar>
2021-01-14 13:09:15 +0100

    Add Material UI Spinner

--- src/js/screens/App.js
+++ src/js/screens/App.js
@@ -4,6 +4,7 @@ import CardList from '../components/CardList/CardList';
 import Layout from '../components/Layout/Layout';
 import Header from '../components/Header/Header';
 import SearchInput from '../components/SearchInput/SearchInput';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { filter } from 'lodash';
 import '../../stlyes/main.scss';
 
@@ -11,25 +12,30 @@ const url = 'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.j
 
 const App = () => {
   const [gnomes, setGnomes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await axios.get(url);
       setGnomes(res.data?.Brastlewark || []);
+      setTimeout(() => setIsLoading(false), 300);
     };
+    /* setIsLoading(true); */
     fetchData();
   }, []);
 
   const searchGnome = () => filter(gnomes, (gnome) => gnome.name.indexOf(searchValue) !== -1);
-
   const filteredGnomes = searchValue === '' ? gnomes : searchGnome();
+
+  const render = isLoading ? <div className='spinner-container'> <CircularProgress size='6rem' /> </div> : <CardList gnomes={filteredGnomes} />
+
 
   return (
     <Layout>
       <Header>Citizens of Brastlewark</Header>
       <SearchInput placeholder='Search for any Gnome' handleChange={(event) => { setSearchValue(event.target.value) }} />
-      <CardList gnomes={filteredGnomes} />
+      {render}
     </Layout>
   )
 }
